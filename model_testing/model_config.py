c35b3a0cd2583c3a3de348c124368c185ec33122
sking115422 <spencerd.king@gmail.com>
2021-12-30 02:22:07 -0500

    "lstm is operational"

--- model_testing/model_config.py
+++ model_testing/model_config.py
@@ -1,12 +1,27 @@
 
 
-asset_pred_list = ['SPY', 'QQQ']
+import pytz
+import datetime as dt
+
+
+
+def get_current_date ():
+    est = pytz.timezone('US/Eastern')
+    now = dt.datetime.now(tz=est)
+
+    time_parts = str(now).split(" ")
+    current_date = time_parts[0]
+    return current_date
+
+
+
+asset_pred_list = ['SPY', 'QQQ', 'IWM']
 
 #Earliest Useable Date: 2001-03-19
 
 train_start = '2000-01-01'
 train_end = '2018-01-01'
 
-test_start = '2018-01-01'
-test_end = '2021-11-01'
+test_start = '2020-01-01'
+test_end = get_current_date()
 
