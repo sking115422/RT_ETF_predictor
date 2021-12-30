0a35d0e9cb028dba7727762967715f6d849f490f
sking115422 <spencerd.king@gmail.com>
2021-12-29 21:58:05 -0500

    process_data notebook is complete

---
+++ model_testing/model_config.py
@@ -0,0 +1,12 @@
+
+
+asset_pred_list = ['SPY', 'QQQ']
+
+#Earliest Useable Date: 2001-03-19
+
+train_start = '2000-01-01'
+train_end = '2018-01-01'
+
+test_start = '2018-01-01'
+test_end = '2021-11-01'
+
